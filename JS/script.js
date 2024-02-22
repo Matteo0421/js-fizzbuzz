43ed11b26cb62d8a7a3b5912e869125242ac8504
Matteo0421 <matteo.carola99@gmail.com>
2024-02-22 15:45:09 +0100

    [error]&&

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+for(let i = 1; i <= 100; i++){
+  if(i % 3 === 0){
+    container.innerHTML += `
+      <div class="box tre">fizz</div>
+    `
+  }else if ( i % 5 ===0){
+    container.innerHTML += `
+      <div class="box cinque">buzz</div>
+    `
+  }else if(i % 3 === 0 && i % 5 ===0){
+    container.innerHTML += `
+      <div class="box treCinque">fizzbuzz</div>
+    `
+  }
+  else{
+    container.innerHTML += `
+      <div class="box ">${i}</div>
+    `
+  }
+}
